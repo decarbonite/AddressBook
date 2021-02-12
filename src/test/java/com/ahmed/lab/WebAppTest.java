1699f85fc8898156c34feac91ec507cd3bdaf623
decarbonite <ahmedromih@cmail.carleton.ca>
2021-02-12 01:31:03 -0500

    Added more tests

--- src/test/java/com/ahmed/lab/WebAppTest.java
+++ src/test/java/com/ahmed/lab/WebAppTest.java
@@ -21,6 +21,9 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 public class WebAppTest {
 
     @Autowired
+    private AddressBookController addressBookController;
+
+    @Autowired
     private MockMvc mvc;
 
 
@@ -49,49 +52,59 @@ public class WebAppTest {
                 .andExpect(MockMvcResultMatchers.jsonPath("$.buddies").exists())
                 .andExpect(MockMvcResultMatchers.jsonPath("$.buddies[0].name").value("Ahmed"))
                 .andExpect(MockMvcResultMatchers.jsonPath("$.buddies[0].phoneNumber").value("12345"));
+        cleanup();
     }
-//
-//    @Test
-//    public void testAddBuddyToAddressBook() throws Exception {
-//        BuddyInfo bud = new BuddyInfo("Blah", "22");
-//
-//        mvc.perform(MockMvcRequestBuilders
-//                .post("/add-buddy")
-//                .param("bookId", String.valueOf(book.getId()))
-//                .content(asJsonString(bud))
-//                .contentType(MediaType.APPLICATION_JSON)
-//                .accept(MediaType.APPLICATION_JSON))
-//                .andDo(print())
-//                .andExpect(status().isCreated())
-//                .andExpect(MockMvcResultMatchers.jsonPath("$.name").value("Blah"))
-//                .andExpect(MockMvcResultMatchers.jsonPath("$.phoneNumber").value("22"));
-//    }
-//
-//    @Test
-//    public void testRemoveAddressBook() throws Exception {
-//        mvc.perform(MockMvcRequestBuilders
-//                .delete("/remove-book")
-//                .param("bookId", String.valueOf(book.getId()))
-//                .accept(MediaType.APPLICATION_JSON))
-//                .andDo(print())
-//                .andExpect(status().isAccepted());
-//    }
-//
-//    @Test
-//    public void testRemoveBuddyInfo() throws Exception {
-////        AddressBook book = new AddressBook();
-////        BuddyInfo bud = new BuddyInfo("Jack", "55555");
-////        book.addBuddy(bud);
-//
-////        addressBookController.createAddressBook(book);
-//
-//        mvc.perform(MockMvcRequestBuilders
-//                .delete("/remove-buddy")
-//                .param("buddyId", String.valueOf(bud.getId()))
-//                .accept(MediaType.APPLICATION_JSON))
-//                .andDo(print())
-//                .andExpect(status().isAccepted());
-//    }
+
+    @Test
+    public void testAddBuddyToAddressBook() throws Exception {
+        AddressBook book = new AddressBook();
+        addressBookController.createAddressBook(book);
+        BuddyInfo bud = new BuddyInfo("Blah", "22");
+
+
+        mvc.perform(MockMvcRequestBuilders
+                .post("/add-buddy")
+                .param("bookId", String.valueOf(book.getId()))
+                .content(asJsonString(bud))
+                .contentType(MediaType.APPLICATION_JSON)
+                .accept(MediaType.APPLICATION_JSON))
+                .andDo(print())
+                .andExpect(status().isCreated())
+                .andExpect(MockMvcResultMatchers.jsonPath("$.name").value("Blah"))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.phoneNumber").value("22"));
+        cleanup();
+    }
+
+    @Test
+    public void testRemoveAddressBook() throws Exception {
+        AddressBook book = new AddressBook();
+        addressBookController.createAddressBook(book);
+
+        mvc.perform(MockMvcRequestBuilders
+                .delete("/remove-book")
+                .param("bookId", String.valueOf(book.getId()))
+                .accept(MediaType.APPLICATION_JSON))
+                .andDo(print())
+                .andExpect(status().isAccepted());
+        cleanup();
+    }
+
+    @Test
+    public void testRemoveBuddyInfo() throws Exception {
+        AddressBook book = new AddressBook();
+        BuddyInfo bud = new BuddyInfo("Jack", "55555");
+        book.addBuddy(bud);
+
+        addressBookController.createAddressBook(book);
+
+        mvc.perform(MockMvcRequestBuilders
+                .delete("/remove-buddy")
+                .param("buddyId", String.valueOf(bud.getId()))
+                .accept(MediaType.APPLICATION_JSON))
+                .andDo(print())
+                .andExpect(status().isAccepted());
+        cleanup();
+    }
 
     private static String asJsonString(final Object obj) {
         try {
@@ -99,5 +112,9 @@ public class WebAppTest {
         } catch (Exception e) {
             throw new RuntimeException(e);
         }
+    }
+
+    private void cleanup() {
+        addressBookController.removeAll();
     }
 }
